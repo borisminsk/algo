3e61f5d495c7d0bff61ce9872c42c3bf2255a7ac
borisminsk <boris_minsk@mail.ru>
2019-02-08 18:16:11 +0300

    add c_n^k

---
+++ c_n^k.cpp
@@ -0,0 +1,10 @@
+int C(int n, int k) {
+    if (n < k) return 0;
+    if (n == k) {
+        return 1;
+    }
+    if (n == 0 || k == 0) {
+        return 1;
+    }
+    return C(n - 1, k - 1) + C(n - 1, k);
+}
